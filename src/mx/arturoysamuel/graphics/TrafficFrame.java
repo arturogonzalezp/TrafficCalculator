7cb9bd1bddc7e8698a8a266f9b44e46d92e9cdf0
Samoz <samosunaz@MacBook-Pro-de-Samuel.local>
2016-04-27 16:46:04 -0500

    Added graphics

--- src/mx/arturoysamuel/graphics/TrafficFrame.java
+++ src/mx/arturoysamuel/graphics/TrafficFrame.java
@@ -1,5 +1,29 @@
 package mx.arturoysamuel.graphics;
 
-public class TrafficFrame {
+import java.awt.BorderLayout;
 
+import javax.swing.JFrame;
+
+public class TrafficFrame extends JFrame {
+	
+	TrafficPanel pnlTraffic = new TrafficPanel(7);
+	InfoPanel pnlInfo = new InfoPanel(12);
+	InputPanel pnlInput = new InputPanel(12);
+	
+	public TrafficFrame() {
+		super("Traffic Calculator");
+		this.add(this.pnlTraffic, BorderLayout.WEST);
+		this.add(this.pnlInfo, BorderLayout.CENTER);
+		this.add(this.pnlInput, BorderLayout.EAST);
+		this.pack();
+		this.setVisible(true);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		TrafficFrame frmTraffic = new TrafficFrame();
+	}
+	
 }
